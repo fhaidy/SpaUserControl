6be2370d2824086c7b377168191aeca2ef23237d
fhaidy <fhaidy@outlook.com>
2020-02-28 17:19:31 -0300

    Created IUserRepository interface

---
+++ SpaUserControl.Domain/Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaUserControl.Domain.Models;
+
+namespace SpaUserControl.Domain.Contracts.Repositories
+{
+    public interface IUserRepository
+    {
+        User Get(string email);
+        User Get(Guid id);
+        void Create(User user);
+        void Update(User user);
+        void Delete(User user);
+    }
+}

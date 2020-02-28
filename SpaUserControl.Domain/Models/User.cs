24cd9b7aa553f2686ebfa41c52eec82e1492bcd3
fhaidy <fhaidy@outlook.com>
2020-02-28 16:31:17 -0300

    Add project files.

---
+++ SpaUserControl.Domain/Models/User.cs
@@ -0,0 +1,51 @@
+﻿using SpaUserControl.Common.Resources;
+using SpaUserControl.Common.Validation;
+using System;
+
+namespace SpaUserControl.Domain.Models
+{
+    public class User
+    {
+        public User(string name, string email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public void SetPassword(string password, string confirmPassword)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, Errors.InvalidPassword);
+            AssertionConcern.AssertArgumentNotNull(confirmPassword, Errors.InvalidPasswordConfirmation);
+            AssertionConcern.AssertArgumentEquals(password, confirmPassword, Errors.PasswordDoNotMatch);
+            AssertionConcern.AssertArgumentLength(password, 6, 20, Errors.InvalidPassword);
+
+            Password = PasswordAssertionConcern.Encrypt(password);
+        }
+
+        public string ResetPassword()
+        {
+            var password = Guid.NewGuid().ToString().Substring(0, 8);
+            Password = PasswordAssertionConcern.Encrypt(password);
+
+            return Password;
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+        }
+
+        public void Validate()
+        {
+            AssertionConcern.AssertArgumentLength(this.Name, 3, 250, Errors.InvalidUserName);
+            EmailAssertionConcern.AssertIsValid(this.Email);
+            PasswordAssertionConcern.AssertIsValid(this.Password);
+        }
+    }
+}
